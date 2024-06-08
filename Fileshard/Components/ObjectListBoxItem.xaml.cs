89d44ad0ba50e096a3db1cdf7ca95a4f784cc637
reaganiwadha <22427233+reaganiwadha@users.noreply.github.com>
2024-06-08 15:48:55 +0700

    Thumbnail and Mutexes

--- Fileshard/Components/ObjectListBoxItem.xaml.cs
+++ Fileshard/Components/ObjectListBoxItem.xaml.cs
@@ -3,15 +3,102 @@ using SkiaSharp;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using System.Windows;
+using Fileshard.Frontend.Helpers;
 
 namespace Fileshard.Frontend.Components
 {
-    public class FileItem
+    public class FileItem : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public BitmapImage Icon { get; set; }
+        private string name;
+        private string path;
+        private BitmapImage icon;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Path
+        {
+            get => path;
+            set
+            {
+                if (path != value)
+                {
+                    path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+        public BitmapImage Icon
+        {
+            get => icon;
+            set
+            {
+                if (icon != value)
+                {
+                    icon = value;
+                    OnPropertyChanged(nameof(Icon));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DrawingMutex
+    {
+        private static DrawingMutex _instance;
+        private static readonly object _lock = new object();
+        private readonly Mutex _mutex;
+
+        private DrawingMutex()
+        {
+            _mutex = new Mutex();
+        }
+
+        public static DrawingMutex Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DrawingMutex();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void WaitOne()
+        {
+            _mutex.WaitOne();
+        }
+
+        public void ReleaseMutex()
+        {
+            _mutex.ReleaseMutex();
+        }
     }
 
     /// <summary>
@@ -19,60 +106,94 @@ namespace Fileshard.Frontend.Components
     /// </summary>
     public partial class ObjectListBoxItem : UserControl
     {
-        Random rnd = new Random();
-
-        public FileItem Item { get; set; }
-
         public ObjectListBoxItem()
         {
             InitializeComponent();
-            DataContext = this;
-            // skElement.InvalidateVisual();
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is FileItem fileItem)
+            {
+                fileItem.PropertyChanged += OnFileItemPropertyChanged;
+                StartDrawingTask(fileItem.Path);
+            }
+
+            if (e.OldValue is FileItem oldFileItem)
+            {
+                oldFileItem.PropertyChanged -= OnFileItemPropertyChanged;
+            }
+        }
+
+        private void OnFileItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(FileItem.Path) && sender is FileItem fileItem)
+            {
+                StartDrawingTask(fileItem.Path);
+            }
+        }
+
+        Boolean isDrawingTurn = false;
+
+        private void StartDrawingTask(string path)
+        {
+            Task.Run(() =>
+            {
+                DrawingMutex.Instance.WaitOne();
+
+                try { 
+                    isDrawingTurn = true;
+                    Application.Current.Dispatcher.Invoke(() => {
+                        skElement.InvalidateVisual();
+                    });
+                } finally
+                {
+                    Thread.Sleep(50);
+                    DrawingMutex.Instance.ReleaseMutex();
+                }
+            });
         }
 
         private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
+            if (isDrawingTurn == false) return;
+
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
 
-
             canvas.Clear(SKColors.White);
 
-            // Your SkiaSharp drawing code here
-            SKPaint paint = new SKPaint
+            if (DataContext is FileItem fileItem && !string.IsNullOrEmpty(fileItem.Path))
             {
-                Style = SKPaintStyle.Fill,
-                Color = SKColors.Blue,
-                IsAntialias = true
-            };
-
-            // Drawing the path text
-            if (Item != null && !string.IsNullOrEmpty(Item.Path))
-            {
-                SKPaint textPaint = new SKPaint
+                try
                 {
-                    Color = SKColors.Black,
-                    IsAntialias = true,
-                    TextAlign = SKTextAlign.Center,
-                    TextSize = 16
-                };
-                // Calculate text bounds
-                SKRect textBounds = new SKRect();
-                textPaint.MeasureText(Item.Path, ref textBounds);
+                    using (var stream = new SKFileStream(fileItem.Path))
+                    using (var bitmap = SKBitmap.Decode(stream))
+                    {
+                        if (bitmap != null)
+                        {
+                            var paint = new SKPaint
+                            {
+                                IsAntialias = false,
+                                FilterQuality = SKFilterQuality.High
+                            };
 
-                // Calculate text position
-                float x = e.Info.Width / 2;
-                float y = e.Info.Height / 2;
+                            canvas.Clear(SKColors.White);
 
-                // Draw text
-                canvas.DrawText(Item.Path, 0, 0, textPaint);
+                            var scaler = new BitmapScaler(bitmap);
+                            var destRect = scaler.CalculateDestinationRect(e.Info.Size);
+
+                            canvas.DrawBitmap(bitmap, destRect, paint);
+                            bitmap.Dispose();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-
-            canvas.DrawCircle(
-                rnd.Next(1, 50),
-                rnd.Next(1, 50),
-                rnd.Next(1, 50), 
-            paint);
         }
     }
 }
---
+++ Fileshard/Helpers/DimentionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Fileshard.Frontend.Helpers
+{
+    public class DimentionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+              object parameter, CultureInfo culture)
+        {
+            return (double)value / double.Parse((string)parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
